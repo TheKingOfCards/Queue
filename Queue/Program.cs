93935fba0eaefb122bcb28fb9e57c5f2f960336b
Samuel Antonsson <samuel.antonsson@elev.ga.ntig.se>
2024-05-24 12:22:19 +0200

    first commit

---
+++ Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿Queue<int> ints = new();
+
+ints.Enqueue(10);
+ints.Enqueue(320);
+ints.Enqueue(20);
+ints.Enqueue(100);
+
+Console.WriteLine("Foreach loop:\n");
+foreach (int number in ints)
+{
+    Console.WriteLine(number);
+}
+
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Dequeue:\n");
+Console.WriteLine(ints.Dequeue());
+Console.WriteLine(ints.Dequeue());
+Console.WriteLine(ints.Dequeue());
+Console.WriteLine(ints.Dequeue());
+
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine("Is queue empty? "+ (ints.Count == 0));
+
+Console.ReadKey();
